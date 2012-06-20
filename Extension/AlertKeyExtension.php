10d81af6a80f8e7b64d5c2b815d6decda442661c
Reece Fowell <reece@codeconsortium.com>
2012-06-20 16:24:25 +0100

    Update flash component for use with twitter bootstrap alerts.

---
+++ Extension/AlertKeyExtension.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of the CCDN CommonBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/> 
+ * 
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNComponent\CommonBundle\Extension;
+
+/**
+ * 
+ * @author Reece Fowell <reece@codeconsortium.com> 
+ * @version 1.0
+ */
+class AlertKeyExtension extends \Twig_Extension
+{
+
+
+
+	/**
+	 *
+	 * @access protected 
+	 */
+	protected $container;
+	
+
+	
+	/**
+	 *
+	 */
+	public function __construct($container)
+	{
+		$this->container = $container;
+	}
+
+
+
+	/**
+	 * 
+	 * @access public
+	 * @return Array()
+	 */
+	public function getFunctions()
+	{
+		return array(
+			'alert_key' => new \Twig_Function_Method($this, 'alertKey'),
+		);
+	}
+
+	
+	
+	/**
+	 *
+	 *
+	 * @access public
+	 */
+	public function alertKey($str)
+	{
+		$key = explode('_', $str);
+		
+		switch($key[0])
+		{
+			case 'notice':
+				$selector = 'flash.notice';
+				break;
+			case 'success':
+				$selector = 'flash.success';
+				break;
+			case 'warning':
+				$selector = 'flash.warning';
+				break;
+			case 'error':
+				$selector = 'flash.error';
+				break;
+			default:
+				$selector = 'flash.warning';
+				break;
+		}
+		
+		return $this->container->get('translator')->trans($selector, array(), 'CCDNComponentCommonBundle');
+	}
+
+	
+	
+	/**
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'alertKey';
+	}
+	
+}
