440822a54fc2be7c6c181197f0976420bb7fe9fd
Reece Fowell <reece@codeconsortium.com>
2012-08-05 05:56:01 +0100

    Update documentation

--- Component/BinSIUnits.php
+++ Component/BinSIUnits.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the CCDN CommonBundle
+ * This file is part of the CCDNComponent CommonBundle
  *
  * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/>
  *
